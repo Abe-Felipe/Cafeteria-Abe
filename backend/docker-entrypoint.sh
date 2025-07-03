f067ae55ed68d73bcad13799a52afbddec5f99ab
Felipe Abe <abe.fydr@gmail.com>
2025-07-03 12:35:00 -0300

    test

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# backend/docker-entrypoint.sh
-
 echo "Entrypoint: Aguardando o banco de dados..."
+
 until npx knex migrate:latest; do
   echo "O banco de dados não está pronto, tentando novamente em 2s..."
   sleep 2
@@ -10,7 +9,9 @@ done
 
 echo "Entrypoint: Migrations executadas com sucesso."
 
-npx knex seed:run && \
-echo "Entrypoint: Seeds executados com sucesso." && \
-echo "Entrypoint: Iniciando o servidor Node.js..." && \
+npx knex seed:run
+
+echo "Entrypoint: Seeds executados com sucesso."
+echo "Entrypoint: Iniciando o servidor Node.js..."
+
 exec node server.js
