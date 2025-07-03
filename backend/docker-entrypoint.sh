4fef874e80b2d5d3f4a852889ef2cda21fe60751
Felipe Abe <abe.fydr@gmail.com>
2025-07-03 12:04:57 -0300

    Finaliza setup Docker e adiciona .env.example

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# backend/docker-entrypoint.sh
+
+echo "Entrypoint: Aguardando o banco de dados..."
+until npx knex migrate:latest; do
+  echo "O banco de dados não está pronto, tentando novamente em 2s..."
+  sleep 2
+done
+
+echo "Entrypoint: Migrations executadas com sucesso."
+
+npx knex seed:run && \
+echo "Entrypoint: Seeds executados com sucesso." && \
+echo "Entrypoint: Iniciando o servidor Node.js..." && \
+exec node server.js
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,16 +1,21 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
     client: 'pg',
     connection: {
-  host: '127.0.0.1',
-  port: 5433,
-  database: 'cafeteria_db',
-  user: 'postgres',
-  password: 'postgres'
-},
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: process.env.DB_PORT || 5432,
+      database: process.env.DB_DATABASE || 'cafeteria_db',
+      user: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD
+    },
     migrations: {
       tableName: 'knex_migrations',
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
 };
